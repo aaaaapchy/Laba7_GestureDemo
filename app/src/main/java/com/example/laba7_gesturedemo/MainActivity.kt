45f4f57a6be0695f0b3ef4042d1848178f9d64ce
aaaaapchy <lidija.verner@yandex.ru>
2025-12-08 00:44:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/laba7_gesturedemo/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.laba7_gesturedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.laba7_gesturedemo.ui.theme.Laba7_GestureDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laba7_GestureDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laba7_GestureDemoTheme {
+        MainScreen()
+    }
+}
