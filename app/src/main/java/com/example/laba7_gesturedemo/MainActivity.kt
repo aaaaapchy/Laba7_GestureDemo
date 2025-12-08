1ba56442793499b303719abf6a7d58edff78c2dc
aaaaapchy <lidija.verner@yandex.ru>
2025-12-08 13:12:17 +0700

    Перетаскивание картинки

--- app/src/main/java/com/example/laba7_gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/laba7_gesturedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
@@ -11,6 +12,9 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,6 +23,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,8 +34,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
@@ -54,9 +63,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    PointerInputDrag(modifier)
+    ScrollModifiers(modifier)
 }
 
+
+@Composable
+fun ScrollModifiers(modifier: Modifier = Modifier) {
+    val image = ImageBitmap.imageResource(id = R.drawable.vacation)
+    Box(modifier = modifier
+        .size(150.dp)
+        .verticalScroll(rememberScrollState())
+        .horizontalScroll(rememberScrollState())) {
+        Canvas(
+            modifier = Modifier
+                .size(360.dp, 270.dp)
+        )
+        {
+            drawImage(
+                image = image,
+                topLeft = Offset(
+                    x = 0f,
+                    y = 0f
+                ),
+            )
+        }
+    }
+}
+
+@Composable
+fun ScrollableModifier(modifier: Modifier = Modifier) {
+    var offset by remember { mutableStateOf(0f) }
+    Box(
+        modifier
+            .fillMaxSize()
+            .scrollable(
+                orientation = Orientation.Vertical,
+                state = rememberScrollableState { distance ->
+                    offset += distance
+                    distance
+                }
+            )
+    ) {
+        Box(modifier = Modifier
+            .size(90.dp)
+            .offset { IntOffset(0, offset.roundToInt()) }
+            .background(Color.Red))
+    }
+}
 @Composable
 fun PointerInputDrag(modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize()) {
